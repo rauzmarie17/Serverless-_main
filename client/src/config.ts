41fc3498455d49a36599812b620956da9e37cf65
rauzmarie17 <onuoharosemary24@gmail.com>
2022-09-04 14:31:21 +0100

    first commit

---
+++ client/src/config.ts
@@ -0,0 +1,8 @@
+const apiId = '9vgrbj77ke';
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-2.amazonaws.com/dev`
+
+export const authConfig = {
+  domain: 'dev-shz-6k2c.us.auth0.com',
+  clientId: 'WJyPqibwTpgMcFpQyj2rIOxoh3zSGb9Q',
+  callbackUrl: 'http://localhost:3000/callback'
+};
